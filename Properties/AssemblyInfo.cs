4a805728939b5701939ff4209a635583d5e2e5d9
Boris Emchenko <yemchenko.b@gmail.com>
2018-05-01 22:30:41 +0300

    1.0.2

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LoggingAndConfig")]
+[assembly: AssemblyDescription("LoggingAndConfig lib")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Boris Emchenko")]
+[assembly: AssemblyProduct("BE.LoggingAndConfig")]
+[assembly: AssemblyCopyright("Boris Emchenko")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cce68746-d8ba-493e-b8d9-e70fd7356d35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
